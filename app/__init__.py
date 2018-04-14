df45edec4a9ca2f64a49a3847436cf32f0f4517c
ramza007 <ramza007@gmail.com>
2018-04-14 17:43:39 +0300

    added minor changes

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,6 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-#from flask_uploads import UploadSet, configure_uploads, IMAGES
-#from flask_mail import Mail
 
 
 bootstrap = Bootstrap()
